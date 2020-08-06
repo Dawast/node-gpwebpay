3733a1ecb77734230c01e9f78c1c865097679eba
David Štuller <david@dswd.cz>
2020-08-06 13:29:43 +0200

    v 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import GpWebpay from './GPWebpay';
+import GpWebpayResponse from './GpWebpayResponse';
+import GpWebpayRequest from './GpWebpayRequest';
 
 export default GpWebpay;
+export { GpWebpay, GpWebpayResponse, GpWebpayRequest };
