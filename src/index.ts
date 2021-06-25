655c819e30bf91439f39e3bd14884ebd6847df85
David Štuller <david@dswd.cz>
2021-06-25 11:34:19 +0200

    ADDINFO - index export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import GpWebpay from './GPWebpay';
 import GpWebpayResponse from './GpWebpayResponse';
 import GpWebpayRequest from './GpWebpayRequest';
+import GpWebpayAddInfo from './GpWebpayAddInfo';
 
 export default GpWebpay;
-export { GpWebpay, GpWebpayResponse, GpWebpayRequest };
+export {
+  GpWebpay,
+  GpWebpayResponse,
+  GpWebpayRequest,
+  GpWebpayAddInfo,
+};
